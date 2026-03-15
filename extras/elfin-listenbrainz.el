f7ff618522816cf9677b408c8502653b9e0750f3
Ad <me@skissue.xyz>
2026-03-15 16:40:41 -0400

    refactor: extras/elfin-scrobble -> extras/elfin-listenbrainz

--- extras/elfin-listenbrainz.el
+++ extras/elfin-listenbrainz.el
@@ -1,4 +1,4 @@
-;;; elfin-scrobble.el --- ListenBrainz scrobbling -*- lexical-binding: t -*-
+;;; elfin-listenbrainz.el --- ListenBrainz scrobbling -*- lexical-binding: t -*-
 
 ;;; Commentary:
 
@@ -9,34 +9,34 @@
 (require 'plz)
 (require 'elfin)
 
-(defgroup elfin-scrobble nil
+(defgroup elfin-listenbrainz nil
   "ListenBrainz scrobbling settings."
   :group 'elfin)
 
 (defcustom elfin-listenbrainz-token nil
   "ListenBrainz user authentication token."
   :type '(choice (const nil) string)
-  :group 'elfin-scrobble)
+  :group 'elfin-listenbrainz)
 
 (defcustom elfin-listenbrainz-url "https://api.listenbrainz.org"
   "ListenBrainz API URL."
   :type 'string
-  :group 'elfin-scrobble)
+  :group 'elfin-listenbrainz)
 
-(defvar elfin-scrobble-mode) ; forward declaration for byte-compiler
+(defvar elfin-listenbrainz-mode) ; forward declaration for byte-compiler
 
-(defvar elfin--current-track nil
+(defvar elfin--listenbrainz-current-track nil
   "Hash table of current track metadata from Jellyfin.")
 
-(defvar elfin--track-start-time nil
+(defvar elfin--listenbrainz-track-start-time nil
   "Unix timestamp when current track started playing.")
 
-(defvar elfin--scrobbled-p nil
+(defvar elfin--listenbrainz-scrobbled-p nil
   "Whether current track has already been scrobbled.")
 
 (defun elfin--listenbrainz-submit (listen-type payload)
   "Submit PAYLOAD to ListenBrainz with LISTEN-TYPE."
-  (when (and elfin-scrobble-mode elfin-listenbrainz-token)
+  (when (and elfin-listenbrainz-mode elfin-listenbrainz-token)
     (let ((url (concat elfin-listenbrainz-url "/1/submit-listens"))
           (body (json-serialize
                  `(:listen_type ,listen-type
@@ -52,7 +52,7 @@
         :else (lambda (err)
                 (message "ListenBrainz submit failed: %S" err))))))
 
-(defun elfin--build-listen-payload (track &optional listened-at)
+(defun elfin--listenbrainz-build-payload (track &optional listened-at)
   "Build ListenBrainz payload from Jellyfin TRACK metadata.
 If LISTENED-AT is provided, include it for scrobble submissions."
   (let* ((artists (gethash "Artists" track))
@@ -88,61 +88,61 @@ If LISTENED-AT is provided, include it for scrobble submissions."
         (setq payload (plist-put payload :listened_at listened-at)))
       payload)))
 
-(defun elfin--scrobble-check-condition (time-pos)
+(defun elfin--listenbrainz-check-condition (time-pos)
   "Check if scrobble condition is met at TIME-POS seconds."
-  (when (and elfin-scrobble-mode
+  (when (and elfin-listenbrainz-mode
              time-pos
-             elfin--current-track
-             (not elfin--scrobbled-p))
-    (let* ((runtime-ticks (gethash "RunTimeTicks" elfin--current-track))
+             elfin--listenbrainz-current-track
+             (not elfin--listenbrainz-scrobbled-p))
+    (let* ((runtime-ticks (gethash "RunTimeTicks" elfin--listenbrainz-current-track))
            (duration-secs (and runtime-ticks (/ runtime-ticks 10000000.0)))
            (threshold (if duration-secs
                           (min (* duration-secs 0.5) 240)
                         240)))
       (when (>= time-pos threshold)
-        (setq elfin--scrobbled-p t)
+        (setq elfin--listenbrainz-scrobbled-p t)
         (elfin--listenbrainz-submit
          "single"
-         (elfin--build-listen-payload
-          elfin--current-track elfin--track-start-time))))))
+         (elfin--listenbrainz-build-payload
+          elfin--listenbrainz-current-track elfin--listenbrainz-track-start-time))))))
 
-(defun elfin--scrobble-on-start-file (track-id)
+(defun elfin--listenbrainz-on-start-file (track-id)
   "Handle file start for scrobbling.
 TRACK-ID is the Jellyfin item ID of the track."
-  (when elfin-scrobble-mode
+  (when elfin-listenbrainz-mode
     (elfin--get (format "/Items/%s" track-id) nil
-      (setq elfin--current-track response)
-      (setq elfin--track-start-time (floor (float-time)))
-      (setq elfin--scrobbled-p nil)
+      (setq elfin--listenbrainz-current-track response)
+      (setq elfin--listenbrainz-track-start-time (floor (float-time)))
+      (setq elfin--listenbrainz-scrobbled-p nil)
       (elfin--listenbrainz-submit
        "playing_now"
-       (elfin--build-listen-payload response)))))
+       (elfin--listenbrainz-build-payload response)))))
 
-(defun elfin--scrobble-on-end-file (_track-id _reason)
+(defun elfin--listenbrainz-on-end-file (_track-id _reason)
   "Handle file end for scrobbling."
-  (setq elfin--current-track nil)
-  (setq elfin--track-start-time nil)
-  (setq elfin--scrobbled-p nil))
+  (setq elfin--listenbrainz-current-track nil)
+  (setq elfin--listenbrainz-track-start-time nil)
+  (setq elfin--listenbrainz-scrobbled-p nil))
 
 ;;;###autoload
-(define-minor-mode elfin-scrobble-mode
+(define-minor-mode elfin-listenbrainz-mode
   "Global minor mode for scrobbling tracks to ListenBrainz."
   :global t
-  :group 'elfin-scrobble
-  (if elfin-scrobble-mode
+  :group 'elfin-listenbrainz
+  (if elfin-listenbrainz-mode
       (progn
         (unless elfin-listenbrainz-token
-          (warn "`elfin-scrobble-mode' enabled without `elfin-listenbrainz-token' set"))
-        (add-hook 'elfin-file-start-hook #'elfin--scrobble-on-start-file)
-        (add-hook 'elfin-file-end-hook #'elfin--scrobble-on-end-file)
-        (elfin-observe-property "time-pos" #'elfin--scrobble-check-condition))
-    (remove-hook 'elfin-file-start-hook #'elfin--scrobble-on-start-file)
-    (remove-hook 'elfin-file-end-hook #'elfin--scrobble-on-end-file)
-    (elfin-unobserve-property "time-pos" #'elfin--scrobble-check-condition)
-    (setq elfin--current-track nil
-          elfin--track-start-time nil
-          elfin--scrobbled-p nil)))
+          (warn "`elfin-listenbrainz-mode' enabled without `elfin-listenbrainz-token' set"))
+        (add-hook 'elfin-file-start-hook #'elfin--listenbrainz-on-start-file)
+        (add-hook 'elfin-file-end-hook #'elfin--listenbrainz-on-end-file)
+        (elfin-observe-property "time-pos" #'elfin--listenbrainz-check-condition))
+    (remove-hook 'elfin-file-start-hook #'elfin--listenbrainz-on-start-file)
+    (remove-hook 'elfin-file-end-hook #'elfin--listenbrainz-on-end-file)
+    (elfin-unobserve-property "time-pos" #'elfin--listenbrainz-check-condition)
+    (setq elfin--listenbrainz-current-track nil
+          elfin--listenbrainz-track-start-time nil
+          elfin--listenbrainz-scrobbled-p nil)))
 
-(provide 'elfin-scrobble)
+(provide 'elfin-listenbrainz)
 
-;;; elfin-scrobble.el ends here
+;;; elfin-listenbrainz.el ends here
